418082cbfe7e6d527bf15315d3bd5d385a035809
huthaifadl <huthaifasyr7@gmail.com>
2025-06-02 07:03:22 +0300

    th project is finish

--- js/script.js
+++ js/script.js
@@ -1,3 +1,48 @@
+var productNameInput = document.getElementById("productName");
+var productPriceInput = document.getElementById("productPrice");
+var productCategoryInput = document.getElementById("productCategory");
+var productImgInput = document.getElementById("productImg");
+var productDescInput = document.getElementById("productDesc");
+var allProduct = [];
+
 function addProduct() {
-    console.log('Product added');
+  var product = {
+    name: productNameInput.value,
+    price: productPriceInput.value,
+    category: productCategoryInput.value,
+    img: productImgInput.value,
+    description: productDescInput.value,
+  };
+
+  allProduct.push(product);
+
+  console.log(allProduct);
+
+  display()
+  clearInputs();
+}
+
+function clearInputs() {
+  productNameInput.value = "";
+  productPriceInput.value = "";
+  productCategoryInput.value = "";
+  productImgInput.value = "";
+  productDescInput.value = "";
+}
+
+function display() {
+  var cartoona = ""
+  for (var i = 0; i < allProduct.length; i++) {
+    cartoona += `
+    <tr>
+              <td>${i + 1}</td>
+              <td><img src="${allProduct[i].img}" alt="" /></td>
+              <td>${allProduct[i].name}</td>
+              <td>${allProduct[i].price}</td>
+              <td>${allProduct[i].category}</td>
+              <td>${allProduct[i].description}</td>
+            </tr>
+    `;
+  }
+  document.getElementById("demo").innerHTML = cartoona
 }
